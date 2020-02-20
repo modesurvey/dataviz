35622959f3b178ff5bfc2c35ad72401b1109afcd
Aaron Lichtner <alichtner@gmail.com>
2020-02-19 20:22:37 -0800

    use the new schema

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dataviz
-Pulls data from firebase and visualizes
+Pulls data from firebase and visualizes. Plots will be generated and saved locally for now.
+
+```python
+python run_viz.py
+```
--- run_viz.py
+++ run_viz.py
@@ -7,6 +7,8 @@ import datetime
 import seaborn as sns
 import time
 import numpy as np
+from collections import defaultdict
+import nordypy
 
 
 def run():
@@ -18,39 +20,57 @@ def run():
 
     firebase = pyrebase.initialize_app(config)
 
-    unit_translation = {'38ed9c286f24': "Cafe Allegro", 
-                        '98798yuah8': "Bulldog Espresso", 
-                        "a03e9e286f24": "Magus Books"}
-
     db = firebase.database()
     resp = db.get()
     data = []
-    for f, v in dict(resp.val())['events'].items():
+    for f, v in dict(resp.val())['accounts'].items():
         data.append(v)
-    data = pd.DataFrame(data)
+
+    # set up all the locations that had a modesurvey box
+    accounts = {}
+    for loc in data:
+        name = loc['name']
+        for k, v in loc['locations'].items():
+            accounts[k] = name
+
+    # get all the events and associate them with an account
+    events = defaultdict(list)
+    for k, v  in dict(resp.val()['streams']).items():
+        location_id = v['location_id']
+        for event_key, event in v['events'].items():
+            events[accounts[location_id]].append(event)
 
     def convert(d):
         try:
             return datetime.datetime.fromtimestamp(int(d))
         except:
             'error'
-
-    data['timestamp'] = data['timestamp'].apply(convert)
-    data['count'] = 1
-    grouped_data = data.groupby(['unit', 'type'])['count'].sum().reset_index()
-
+    
     list_ordering = ["walk", "wheels", 'transit', 'car']  
 
-    grouped_data["type"] = pd.Categorical(grouped_data["type"], categories=list_ordering, ordered=True)
-    grouped_data["unit"] = grouped_data['unit'].apply(lambda k: unit_translation[k])
+    # aggregate to all responses per account 
+    all_data = pd.DataFrame()
+    for k, v in events.items():
+        data = pd.DataFrame(v)
 
+        data['timestamp'] = data['timestamp'].apply(convert)
+        data['count'] = 1
+        print(f'Total responses: {len(data)}')
+        grouped_data = data.groupby(['type'])['count'].sum().reset_index()
+
+
+        grouped_data["type"] = pd.Categorical(grouped_data["type"], categories=list_ordering, ordered=True)
+        grouped_data["account"] = k 
+        all_data = pd.concat([all_data, grouped_data])
+
+    # plot everything 
     sns.set_style("whitegrid")
     # Create an array with the colors you want to use
     colors = ['#66b3ff','#99ff99','#ff9999','#808080']
     # Set your custom color palette
     customPalette = sns.set_palette(sns.color_palette(colors))
 
-    g = sns.catplot(x="type", y="count", col="unit",
+    g = sns.catplot(x="type", y="count", col="account",
                     data=grouped_data, saturation=.5,
                     kind="bar", aspect=1, palette=customPalette)
     (g.set_xticklabels(["walk", "wheels", "transit", "car"])
@@ -61,13 +81,13 @@ def run():
 
     # Pie chart
 
-    for u in grouped_data['unit'].unique():
-        mini = grouped_data[grouped_data['unit'] == u]
+    for u in all_data['account'].unique():
+        mini = all_data[all_data['account'] == u]
         labels = mini['type']
         sizes = mini['count']
         
-        #colors
-        colors = ['#808080','#ff9999','#99ff99','#66b3ff']
+        #colors : car transit wheels walk
+        colors = ['#808080','#ff9999','#66b3ff', '#99ff99']
 
         fig1, ax1 = plt.subplots()
         ax1.pie(sizes, colors = colors, labels=labels, autopct='%1.1f%%', startangle=90, pctdistance=.55)
@@ -78,9 +98,21 @@ def run():
         # Equal aspect ratio ensures that pie is drawn as a circle
         plt.title(u)
         ax1.axis('equal')  
+        fig.text(.5, .01, 'Updated at: ' + str( datetime.datetime.now().strftime("%Y-%m-%d %I:%M %p")  ), 
+                  ha='center', fontsize=10)
+
         plt.tight_layout()
-        plt.savefig(f'{u}_circle.png')
+        path = 'output/'
+        path = path + '_'.join(u.lower().split(' ')) + '.png'
+        print(u)
+        plt.savefig(path)
         plt.show()
+        plt.close()
+        nordypy.s3_upload(bucket='modesurvey-dataviz', s3_filepath=path, 
+                                                      local_filepath=path,
+                                                      permission='public-read')
+
+    
 
 if __name__ == '__main__':
     run()
